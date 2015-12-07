c5f481bd356bf3025f12cda085d7cb15f0f0a5db
JMHockham <jmhockham@gmail.com>
2015-12-07 16:04:18 +0000

    initialcommit, added basic App class to run the clock.

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+
+public class App {
+	
+	private SimpleDateFormat dateFormat;
+	private static Calendar calendar;
+
+	public App() {
+		dateFormat = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss");
+		calendar = new GregorianCalendar();
+	}
+	
+	public static void main(String[] args) {
+		App a = new App();
+		System.out.println("The current date/time is:" + calendar.getTime());
+		System.out.println("seconds diplayed here");
+		System.out.println("first row displayed here");
+		System.out.println("second row displayed here");
+		System.out.println("third row displayed here");
+		System.out.println("fourth row displayed here");
+	}
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void testGetDateFormat() {
+		assertTrue(false);
+	}
+
+	@Test
+	public void testGetCalendar() {
+		assertTrue(false);
+	}
+}
